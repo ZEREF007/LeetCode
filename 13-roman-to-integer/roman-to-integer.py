970cb232f6e58e0c14aff3b374e02211b4b93fdf
Shiva Raj Bhatt <49589334+ZEREF007@users.noreply.github.com>
2025-10-16 01:44:32 +0800

    Time: 4 ms (63.88%) | Memory: 17.8 MB (64.44%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,21 +1,9 @@
 class Solution:
-
-    def romanToInt(self, inp: str) -> int:
-        roman = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-        }
-
-        result = 0
-        for i in range(len(inp)):
-            if i + 1 < len(inp) and roman[inp[i]] < roman[inp[i + 1]]:
-                result -= roman[inp[i]]
-            else:
-                result += roman[inp[i]]
-                
-        return result
+    def romanToInt(self, s: str) -> int:
+        hash_map = {'I':1, 'V':5,'X':10, 'L':50, 'C':100, 'D':500,'M':1000}
+        total,prev = 0,0
+        for char in reversed(s): # OR s[::-1] 
+            current= hash_map[char]
+            total += current if current >= prev else -current
+            prev = current
+        return total
